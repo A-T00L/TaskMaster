d4f4456950242f3cae87d8d3e77a8660ecb72658
Atul Choudhary <atul.0798choudhary@gmail.com>
2024-07-18 01:52:26 +0530

    Add project files.

---
+++ ToDo/Models/ToDoo.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using System.ComponentModel.DataAnnotations;
+
+namespace ToDo.Models
+{
+    public class ToDoo
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage = "Please enter a description.")]
+        public string Description { get; set; } = string.Empty;
+        [Required(ErrorMessage = "Please enter a due date.")]
+        public DateTime? DueDate { get; set; }
+        [Required(ErrorMessage = "Please select a category.")]
+        public string CategoryId { get; set; } = string.Empty;
+        [ValidateNever]
+        public Category Category { get; set; } = null!; // We will bring in CategoryId basically as a foreign Key to this object
+        [Required(ErrorMessage = "Please select a Status")]
+        public string StatusId { get; set; } = string.Empty;
+        [ValidateNever]
+        public Status Status { get; set; } = null!;
+        public bool Overdue => StatusId == "open" && DueDate < DateTime.Today;
+    }
+}
